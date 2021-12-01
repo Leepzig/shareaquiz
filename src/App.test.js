557f999909de53a6d90eb2a5d8b1030778587307
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2021-11-30 21:16:18 -0600

    added tests folder and login and signup test files

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,5 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/welcome to share a quiz/i);
-  expect(linkElement).toBeInTheDocument();
+  render(<Signup />)
 });
