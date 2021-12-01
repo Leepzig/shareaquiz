d7d26f8d19a2f4c8c6f7bf0eccb74912a4f69379
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2021-11-30 21:37:30 -0600

    added very basic signup tests

--- src/components/_tests_/Signup.test.js
+++ src/components/_tests_/Signup.test.js
@@ -6,8 +6,12 @@ import Signup from '../sessions/Signup';
 
 test('has a header of Please Signup', () => {
   render(<Signup />);
-
   const header = screen.getByRole('heading', { name: 'Please Signup' });
-
   expect(header).toBeInTheDocument();
+})
+
+test('has a button that says Create Account', () => {
+  render(<Signup />);
+  const button = screen.getByRole('button', { name: 'Create Account' });
+  expect(button).toBeInTheDocument();
 })
