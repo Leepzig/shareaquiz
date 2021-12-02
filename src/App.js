24ed6c2f34ae58e0713ceeabcefd0f0042bc4b79
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2021-12-01 23:19:22 -0600

    added react router dom stuff to app.js for login, testing material up template with login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
 import './App.css';
+import Login from './components/sessions/Login';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Share A Quiz!</h1>
+      <Router>
+        <Routes>
+          <Route exact path = '/login' element={<Login />} />
+        </Routes>
+      </Router>
 
     </div>
   );
