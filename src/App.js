5b3df054c1bf8b787be91f8b8ae12a382e04b761
andrewsimmons <neon.duncan@gmail.com>
2021-12-04 19:42:59 -0600

    added navbar and implmented basic navigation for login and new account page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import './App.css';
 import Login from './components/sessions/Login';
 import Signup from './components/sessions/Signup';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Navbar from './components/navigation/Navbar';
+import NewQuiz from './components/Quizes/NewQuiz'
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Share A Quiz!</h1>
       <Router>
+        <Navbar />
+      <h1>Welcome to Share A Quiz!</h1>
         <Routes>
           <Route exact path = '/login' element={<Login />} />
-          <Route exact path = '/signup' element={<Signup />} />
+          <Route exact path = '/newaccount' element={<Signup />} />
+          <Route exact path = '/newquiz' element={<NewQuiz />} />
         </Routes>
       </Router>
 
