5fba961b0b150429c0b06285e5c663f823efac40
andrewsimmons <neon.duncan@gmail.com>
2021-12-10 12:00:16 -0600

    added a redirect after submitting a new quiz to redirect user to home page

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route exact path = '/' element={<Home />} />
+          <Route exact path = '/home' element={<Home />} />
           <Route exact path = '/login' element={<Login />} />
           <Route exact path = '/newaccount' element={<Signup />} />
           <Route exact path = '/newquiz' element={<NewQuiz />} />
--- src/components/navigation/Home.js
+++ src/components/navigation/Home.js
@@ -4,17 +4,17 @@ import React from 'react'
 const Home = () => {
     return (
         <>
-        <Box
+        {/* <Box
             sx={{
                 marginTop: 8,
                 display: 'flex',
                 flexDirection: 'column',
                 alignItems: 'center',
-              }}>
+              }}> */}
 
         <Typography variant='h2'>Welcome to Share A Quiz!</Typography>
         <Typography >Create a new quiz or find one already made and start studying!</Typography>
-        </Box>
+        {/* </Box> */}
         </>
     )
 }
