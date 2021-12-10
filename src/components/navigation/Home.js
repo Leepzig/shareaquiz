1c8a58f2c819769f0061b76fe192a8a18b46cfd2
andrewsimmons <neon.duncan@gmail.com>
2021-12-10 11:43:03 -0600

    added Home component to navigation

---
+++ src/components/navigation/Home.js
@@ -0,0 +1,22 @@
+import { Typography, Box } from '@mui/material'
+import React from 'react'
+
+const Home = () => {
+    return (
+        <>
+        <Box
+            sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}>
+
+        <Typography variant='h2'>Welcome to Share A Quiz!</Typography>
+        <Typography >Create a new quiz or find one already made and start studying!</Typography>
+        </Box>
+        </>
+    )
+}
+
+export default Home
